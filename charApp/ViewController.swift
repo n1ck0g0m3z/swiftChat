925dd4d5ce15ef0b6b7677471884ba4937a0c9dd
Student <student@m303-20.local>
2016-05-21 09:51:12 +0900

    Initial Commit

---
+++ charApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  charApp
+//
+//  Created by Student on 2016/05/21.
+//  Copyright © 2016年 N1ck0. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
